e5c528229455e7192d28656d25e270da8fba6ffc
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-02-25 05:14:10 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'products/index', to: 'products#index'
+
+  get 'products/add', to: 'products#add'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
